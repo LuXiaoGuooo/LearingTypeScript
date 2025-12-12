a343a4f55ab4dc2cd06029461ce01ef79ef41c32
陆小果 <luxiaoguo@foxmail.com>
2025-12-12 09:49:43 +0800

    init

---
+++ 1.Day/1.ts类型缺失.ts
@@ -0,0 +1,6 @@
+function getLength(args: string | string[]){
+    return args.length
+}
+
+getLength(["我", "爱","你"])
+getLength("科比")
---
+++ 1.Day/2.HelloWorld.ts
@@ -0,0 +1,8 @@
+  // string 才是ts提供的标识符
+  // String 是来自于js的包装类
+ let message: string = "Hello world"
+ message = "2025年12月11日，我站在迷雾之中，看不清未来的路"
+
+console.log(message)
+
+export {}
