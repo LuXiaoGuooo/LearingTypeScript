7c500d7ae9e22e17ce791043196a86fe844a4a9f
陆小果 <luxiaoguo@foxmail.com>
2025-12-12 10:02:14 +0800

    init

--- 1.Day/1.ts类型缺失.ts
+++
@@ -1,6 +0,0 @@
-function getLength(args: string | string[]){
-    return args.length
-}
-
-getLength(["我", "爱","你"])
-getLength("科比")
--- 1.Day/2.HelloWorld.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// string 才是ts提供的标识符
-// String 是来自于js的包装类
-var message = "Hello world";
